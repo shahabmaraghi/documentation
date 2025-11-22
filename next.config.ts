63e638eba7f79523dacf1f48651463980092dc59
sh.maraghi <shahab.maraghi@zebline.com>
2025-11-22 19:53:51 +0330

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import nextra from 'nextra'
 
-const withNextra = nextra({})
+const withNextra = nextra({
+  latex: true,
+  search: {
+    codeblocks: false
+  },
+})
 
-export default withNextra({})
+export default withNextra({
+  reactStrictMode: true,
+  transpilePackages: ['nextra', 'nextra-theme-docs'],
+})
